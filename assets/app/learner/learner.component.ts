d7627e8cc0a33504fd90e3cfc98deb891faa73d1
VincentZhouGit <vzzhvincent@gmail.com>
2016-11-12 18:41:14 -0500

    push stylesheet

--- assets/app/learner/learner.component.ts
+++ assets/app/learner/learner.component.ts
@@ -4,6 +4,10 @@ import { Component } from '@angular/core';
     selector: 'app-learner',
     template: `
         <h3>Hello, this is the learner page.</h3>
+        
+        <div class="col-md-8 col-md-offset-2">
+            <button class="btn btn-primary" (click)="Purchase()">Purchase</button>
+        </div>
     `
 })
 export class LearnerComponent {}
---
+++ assets/app/learner/learner.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
+
+@Injectable()
+export class LearnerService {
+    constructor(private http: Http) {}
+
+
+}
